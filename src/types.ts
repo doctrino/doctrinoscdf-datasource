b1b5fd63e8ad380f482f12d2d4c7aec4b402d4ff
anders-albert <anders0albert@gmail.com>
2026-05-18 19:12:04 +0200

    refactor: set op options

--- src/types.ts
+++ src/types.ts
@@ -19,16 +19,31 @@ export interface DataSourceResponse {
   datapoints: DataPoint[];
 }
 
+export type LoginFlow = 'token' | 'clientCredentials' | 'deviceCode'
+export type LoginMode = 'manual' | 'guided'
 /**
  * These are options configured for each DataSource instance
  */
 export interface MyDataSourceOptions extends DataSourceJsonData {
-  path?: string;
+  loginFlow: LoginFlow;
+  mode:  LoginMode;
+  cdfCluster?: string;
+  cdfProject?: string;
+  cdfUrl?: string;
+  clientId?: string;
+  idpProvider?: string;
+  idpTokenURL?: string;
+  idpTenantID?: string;
+  idpScopes?: string[];
+  idpDiscoveryURL?: string;
+  idpAudienceURL?: string;
+  idpAuthorityURL?: string;
 }
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
  */
 export interface MySecureJsonData {
-  apiKey?: string;
+  token?: string;
+  clientSecret?: string;
 }
