cc529adac55967deddb7200639940cbc5359bfa3
anders-albert <anders0albert@gmail.com>
2026-06-01 09:39:38 +0200

    refactor: added instance upsert

--- pkg/cdf/instances.go
+++ pkg/cdf/instances.go
@@ -29,6 +29,15 @@ type InstanceItemResponse struct {
 	Items []InstanceResponse `json:"items"`
 }
 
+type InstanceAggregateRequest struct {
+	View       ViewId           `json:"view"`
+	Aggregates []AggregateItem  `json:"aggregates,omitempty"`
+	Query      string           `json:"query,omitempty"`
+	Properties []string         `json:"properties,omitempty"`
+	Filter     []map[string]any `json:"filter,omitempty"`
+	Limit      int              `json:"limit,omitempty"`
+}
+
 type AggregateValue struct {
 	Aggregate string  `json:"aggregate"`
 	Property  string  `json:"property"`
@@ -42,10 +51,6 @@ type AggregateResponse struct {
 
 type InstanceAggregateResponse struct {
 	Items []AggregateResponse `json:"items"`
-}
-
-type instances struct {
-	apiClient *apiClient
 }
 
 type AggregateProperty struct {
@@ -68,6 +73,47 @@ type InstanceSearchRequest struct {
 	Limit      int              `json:"limit,omitempty"`
 }
 
+type InstanceSlimItem struct {
+	InstanceType    string `json:"instanceType"`
+	Version         int    `json:"version"`
+	WasModified     bool   `json:"wasModified"`
+	Space           string `json:"space"`
+	ExternalId      string `json:"externalId"`
+	CreatedTime     int64  `json:"createdTime"`
+	LastUpdatedTime int64  `json:"lastUpdatedTime"`
+}
+
+type InstanceSlimResponse struct {
+	Items []InstanceSlimItem `json:"items"`
+}
+
+type InstanceData struct {
+	Source     ViewId         `json:"source"`
+	Properties map[string]any `json:"properties"`
+}
+
+type InstanceRequest struct {
+	InstanceType    string         `json:"instanceType"`
+	ExistingVersion *int           `json:"existingVersion,omitempty"`
+	Space           string         `json:"space"`
+	ExternalId      string         `json:"externalId"`
+	Type            *InstanceId    `json:"type,omitempty"`
+	Sources         []InstanceData `json:"sources,omitempty"`
+}
+
+type InstanceUpsertRequest struct {
+	Items                     []InstanceRequest `json:"items"`
+	AutoCreateDirectRelations *bool             `json:"autoCreateDirectRelations,omitempty"`
+	AutoCreateStartNodes      *bool             `json:"autoCreateStartNodes,omitempty"`
+	AutoCreateEndNodes        *bool             `json:"autoCreateEndNodes,omitempty"`
+	SkipOnVersionConflict     *bool             `json:"skipOnVersionConflict,omitempty"`
+	Replace                   *bool             `json:"replace,omitempty"`
+}
+
+type instances struct {
+	apiClient *apiClient
+}
+
 func (i *instances) Search(ctx context.Context, request InstanceSearchRequest) ([]InstanceResponse, error) {
 	data, err := json.Marshal(request)
 	if err != nil {
@@ -84,15 +130,6 @@ func (i *instances) Search(ctx context.Context, request InstanceSearchRequest) (
 	return resp.Items, nil
 }
 
-type InstanceAggregateRequest struct {
-	View       ViewId           `json:"view"`
-	Aggregates []AggregateItem  `json:"aggregates,omitempty"`
-	Query      string           `json:"query,omitempty"`
-	Properties []string         `json:"properties,omitempty"`
-	Filter     []map[string]any `json:"filter,omitempty"`
-	Limit      int              `json:"limit,omitempty"`
-}
-
 func (i *instances) Aggregate(ctx context.Context, request InstanceAggregateRequest) ([]AggregateResponse, error) {
 	data, err := json.Marshal(request)
 	if err != nil {
@@ -105,6 +142,22 @@ func (i *instances) Aggregate(ctx context.Context, request InstanceAggregateRequ
 	var resp InstanceAggregateResponse
 	if err := json.Unmarshal(body, &resp); err != nil {
 		return nil, fmt.Errorf("cdf /models/instances/aggregate: %w", err)
+	}
+	return resp.Items, nil
+}
+
+func (i *instances) Upsert(ctx context.Context, request InstanceUpsertRequest) ([]InstanceSlimItem, error) {
+	data, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+	body, err := i.apiClient.doBody(ctx, http.MethodPost, "/models/instances", bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	var resp InstanceSlimResponse
+	if err := json.Unmarshal(body, &resp); err != nil {
+		return nil, fmt.Errorf("cdf /models/instances/upsert: %w", err)
 	}
 	return resp.Items, nil
 }
