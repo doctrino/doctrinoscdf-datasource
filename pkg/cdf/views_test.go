a6f48c388485867cfd224f79dba5589505306e83
anders-albert <anders0albert@gmail.com>
2026-06-04 19:12:51 +0200

    refactor: added test

---
+++ pkg/cdf/views_test.go
@@ -0,0 +1,17 @@
+package cdf
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestViews_Retrieve(t *testing.T) {
+	ctx := context.Background()
+	viewId := ViewId{"view", "cdf_cdm", "CogniteTimeSeries", "v1"}
+	resp, err := testClient.Views.Retrieve(ctx, ViewRetrieveRequest{[]ViewId{viewId}})
+	require.NoError(t, err)
+	require.NotNil(t, resp)
+	require.Greater(t, len(resp), 0)
+}
