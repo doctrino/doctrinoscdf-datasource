cde2f5d16d6ad6351ed3a2a606f5283a85455f11
Anders Albert <60234212+doctrino@users.noreply.github.com>
2026-06-05 09:13:37 +0200

    View retrieve backend (#29)
    * feat; added view retrieve to backend
    
    * refactor: added test
    
    * refactor: register
    
    * fix: set views api
    
    * fix: type optional

--- pkg/cdf/views.go
+++ pkg/cdf/views.go
@@ -1,8 +1,63 @@
 package cdf
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type ViewId struct {
-	Type       string `json:"type"`
+	Type       string `json:"type,omitempty"`
 	Space      string `json:"space"`
 	ExternalId string `json:"externalId"`
 	Version    string `json:"version"`
+}
+
+type ViewRetrieveRequest struct {
+	Items []ViewId `json:"items"`
+}
+
+type ViewResponse struct {
+	Space            string         `json:"space"`
+	ExternalId       string         `json:"externalId"`
+	Version          string         `json:"version"`
+	Name             *string        `json:"name,omitempty"`
+	Description      *string        `json:"description,omitempty"`
+	Filter           map[string]any `json:"filter,omitempty"`
+	Implements       []ViewId       `json:"implements,omitempty"`
+	CreatedTime      int64          `json:"createdTime"`
+	LastUpdatedTime  int64          `json:"lastUpdatedTime"`
+	Writable         bool           `json:"writable"`
+	Queryable        bool           `json:"queryable"`
+	UsedFor          string         `json:"usedFor"`
+	IsGlobal         bool           `json:"isGlobal"`
+	Properties       map[string]any `json:"properties,omitempty"`
+	MappedContainers []ContainerId  `json:"mappedContainers,omitempty"`
+}
+
+type ViewItemsResponse struct {
+	Items []ViewResponse `json:"items"`
+}
+
+type views struct {
+	apiClient *apiClient
+}
+
+func (v *views) Retrieve(ctx context.Context, request ViewRetrieveRequest) ([]ViewResponse, error) {
+	data, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+	body, err := v.apiClient.doBody(ctx, http.MethodPost, "/models/views/byids", bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	var resp ViewItemsResponse
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, fmt.Errorf("cdf /models/views/byids: %w", err)
+	}
+	return resp.Items, nil
 }
