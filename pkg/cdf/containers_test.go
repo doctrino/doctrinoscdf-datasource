b763b3bcc35daf63b1efb13fc348f8e1a28f6741
anders-albert <anders0albert@gmail.com>
2026-06-24 19:55:58 +0200

    tests: added test for container

--- pkg/cdf/containers_test.go
+++ pkg/cdf/containers_test.go
@@ -27,3 +27,40 @@ func TestContainers_InspectWithArgs(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, resp)
 }
+
+func strPtr(s string) *string { return &s }
+
+func TestContainers_Upsert(t *testing.T) {
+	ctx := context.Background()
+	containerRequest := ContainerCreateRequest{
+		Items: []ContainerRequest{
+			{
+				Space:       testSpace,
+				ExternalId:  "SensorContainer",
+				Name:        strPtr("Sensor Container"),
+				Description: strPtr("This is part of the Grafana plugin"),
+				UsedFor:     strPtr("node"),
+				Properties: map[string]any{
+					"location": map[string]any{
+						"type": map[string]any{
+							"type": "text",
+						},
+					},
+				},
+				Constraints: map[string]any{
+					"timeSeriesPresent": map[string]any{
+						"constraintType": "requires",
+						"require": map[string]string{
+							"type":       "container",
+							"space":      "cdf_cdm",
+							"externalId": "CogniteTimeSeries",
+						},
+					},
+				},
+			},
+		},
+	}
+	containerResponses, err := testClient.Containers.Upsert(ctx, containerRequest)
+	require.NoError(t, err)
+	require.NotNil(t, containerResponses)
+}
